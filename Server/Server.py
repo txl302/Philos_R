82b11407facf883aa1151d36897504857de08f91
txl302 <txl302@case.edu>
2018-06-19 22:42:28 -0400

    test

---
+++ Server/Server.py
@@ -0,0 +1,20 @@
+import threading
+
+def init():
+	flag_v = 0
+	flag_m = 0
+	flag_a = 0
+
+	if flag_a == 0:
+		print "audio parts missing"
+
+	if flag_m == 0:
+		pass
+
+def module_detection():
+	pass
+
+
+while True:
+	pass
+	
