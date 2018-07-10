71609e9e1d740baf89398a9da2a066843e0c7983
txl302 <txl302@case.edu>
2018-07-10 15:22:54 -0400

    setup

---
+++ Functions/video/function.py
@@ -0,0 +1,68 @@
+import socket; 
+import threading; 
+import struct; 
+import os; 
+import time; 
+import sys; 
+import numpy 
+import cv2
+import re 
+
+class webCamConnect: 
+	def __init__(self, resolution = [640,480], remoteAddress = ("192.168.1.183", 7999), windowName = "Taoge Niubi"): 
+		self.remoteAddress = remoteAddress; 
+		self.resolution = resolution; 
+		self.name = windowName; 
+		self.mutex = threading.Lock(); 
+		self.src=911+15 
+		self.interval=0 
+		self.path=os.getcwd() 
+		self.img_quality = 15 
+	def _setSocket(self): 
+		self.socket=socket.socket(socket.AF_INET, socket.SOCK_STREAM); 
+		self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1); 
+	def connect(self): 
+		self._setSocket(); 
+		self.socket.connect(self.remoteAddress); 
+	def _processImage(self): 
+		self.socket.send(struct.pack("lhh",self.src,self.resolution[0],self.resolution[1])); 
+		while(1): 
+			info = struct.unpack("lhh",self.socket.recv(12)); 
+			bufSize = info[0]; 
+			if bufSize: 
+				try: 
+					self.mutex.acquire(); 
+					self.buf=b'' 
+					tempBuf=self.buf; 
+					while(bufSize):
+						tempBuf = self.socket.recv(bufSize); 
+						bufSize -= len(tempBuf); 
+						self.buf += tempBuf; 
+						data = numpy.fromstring(self.buf,dtype='uint8') 
+						self.image=cv2.imdecode(data,1) 
+						cv2.imshow(self.name,self.image) 
+				except: 
+					print("receive failed") 
+					pass; 
+				finally: 
+					self.mutex.release(); 
+					if cv2.waitKey(10) == 27: 
+						self.socket.close() 
+						cv2.destroyAllWindows() 
+						print("abandon connection") 
+						break 
+
+
+	def setWindowName(self, name): 
+		self.name = name; 
+	def setRemoteAddress(remoteAddress): 
+		self.remoteAddress = remoteAddress; 
+
+
+def main(): 
+	print("create connection") 
+	cam = webCamConnect(); 
+	cam.connect(); 
+	cam._processImage();
+if __name__ == "__main__": 
+	main();
---
+++ Master/master_client.py
@@ -0,0 +1,92 @@
+import socket
+import threading
+
+table_robot = []
+table_function = []
+
+def listen_robot():
+	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	s.bind(('127.0.0.1', 8013))
+
+	data, addr = s.recvfrom(1024)
+	print 'Received from %s:%s.' %addr
+	print 'Robot %s online' %data
+
+
+def listen_function():
+
+	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	s.bind(('127.0.0.1',8014))
+
+	data, addr = s.recvfrom(1024)
+
+	if data == 'connect':
+		print 'Received from %s:%s.' %addr
+		print 'function %s online' %data
+
+		s.sendto('Hello, %s!' % data, addr)
+
+
+
+		table_function.append(data)
+
+	if data == 'disconnect':
+		print 'Function %s disconnect from the server' %data
+		print 'Function %s '
+
+def robot_thread():
+
+	while True:
+		listen_robot()
+
+
+def function_thread():
+
+	while True:
+		listen_function()
+
+def command():
+
+	global table_robot
+	global table_function
+
+	while True:
+		str = raw_input()
+
+		if str == 'robot':
+			print 'The number of connected robot: %s' %len(table_robot)
+			print table_robot
+
+		if str == 'function':
+			print 'The number of online function: %s' %len(table_function)
+			print table_function
+
+		if str == 'command':
+			print 'command'
+
+		if str == 'help':
+			print 'robot'
+
+		else:
+			print 'enter "help" for more command'
+
+
+
+def test():
+	#t_robot = threading.Thread(target = listen_robot, name = 'robot_loop')
+	t_function = threading.Thread(target = listen_function, name = 'function_loop')
+
+	t_command = threading.Thread(target = command, name = 'command_loop')
+
+	t_function.start()
+	#t_robot.start()
+	t_command.start()
+
+	t_function.join()
+	#t_robot.join()
+	t_command.join()
+
+
+
+if __name__ == '__main__':
+	test()
