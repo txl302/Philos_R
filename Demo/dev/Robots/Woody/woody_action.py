57afd75b3ea13e62294eb319ae892dbfcec87e5a
XiangyiCheng <xxc283@case.edu>
2018-12-03 01:46:49 -0500

    t

--- Demo/dev/Robots/Woody/woody_action.py
+++ Demo/dev/Robots/Woody/woody_action.py
@@ -26,7 +26,7 @@ if len(found_ids) < 2:
     raise IOError('You should connect at least two motors on the bus for this test.')
 #chose all motors and enable torque and set the same speed
 ids = found_ids[:]
-dxl_io.enable_torque(ids)
+#dxl_io.enable_torque(ids)
     
 def move_to(m_id, speed, pose):
     dxl_io.set_moving_speed(dict(zip(m_id, itertools.repeat(speed))))
@@ -53,11 +53,9 @@ def run_guss(m_id, current_pos, des_pos, time_inv):
 				speed.append((des_pos[i] - current_pos[i])*Guss(current_time, 1, time.time() - time_inv/2))
 				pose.append(current_pos[i] + (des_pos[i] - current_pos[i])*(time.time() - current_time)/(des_time - current_time))
 			#move_to(m_id, pose, speed)
-			print pose, speed
+			print(pose, speed)
 		else:
 			break
-
-def triangle():
 
 
 def run_tri(m_id, current_pos, des_pos, time_inv):
@@ -101,7 +99,7 @@ def run_tri(m_id, current_pos, des_pos, time_inv):
 
 		move_to(m_id, pose, speed)
 
-	print pose
+	print(pose)
 
 pos1 = [1.32, -1.83]
 pos2 = [-86.07, 87.54]
@@ -111,7 +109,7 @@ ids = [3, 8]
 
 if __name__ == '__main__':
 	while True:
-		run(ids, pos1, pos2, 6)
+		run_tri(ids, pos1, pos2, 6)
 		time.sleep(3)
-		run(ids, pos2, pos1, 6)
+		run_tri(ids, pos2, pos1, 6)
 		time.sleep(3)
--- Demo/dev/Robots/woody2.py
+++ Demo/dev/Robots/woody2.py
@@ -8,15 +8,19 @@ import itertools
 from random import randint
 
 from Woody import woody_action
-
 m_ids = [1,2,3,4,5,6,7,8,9,10,11,12]
+speed = [80, 80, 80, 80, 80, 80, 80, 80, 80, 80, 80, 80]
+
+move1 = 0.0
+move2 = 0.0
 
 vision_server = ("192.168.1.87", 9901)
 
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.bind(("192.168.1.237", 9901))
 
 s_vision = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 def camera_send():
     while True:
@@ -24,15 +28,14 @@ def camera_send():
         s_vision.sendto(imgencode, vision_server)
         cv2.imshow('img', imgencode)
 
-def run():
-	while True:
-		p = woody_action.get_present_position(m_ids)
-		m_pos = str(p)
-		print m_pos
-		#s.sendto(p, ("192.168.1.87", 9901))
+        
+def reveice_move(s,sc):
+	data,addr = s.recvfrom(64000)
+	data = numpy.fromstring(data, dtype = 'uint8')
+        move(m_ids, data, speed)
 
 
 
 	
 if __name__ == '__main__':
-	run()
+    receive_move(s)
