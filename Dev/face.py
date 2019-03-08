8e5e88d454f9bc7d04067e0e83c9b30817d3f27d
txl302 <txl302@case.edu>
2019-03-07 16:46:44 -0800

    face change test

--- Dev/face.py
+++ Dev/face.py
@@ -1,8 +1,11 @@
 import numpy as np
 import cv2
 import screeninfo
- 
-if __name__ == '__main__':
+import random
+import time
+
+
+def expression_rob(sel):
     screen_id = 0
     is_color = False
  
@@ -10,7 +13,15 @@ if __name__ == '__main__':
     screen = screeninfo.get_monitors()[screen_id]
     width, height = screen.width, screen.height
 
-    image = cv2.imread('expression/normal.png')
+    if(sel == 1):
+        image = cv2.imread('expression/normal.png')
+    elif(sel == 2):
+        image = cv2.imread('expression/angry.png')
+    elif(sel == 3):
+        image = cv2.imread('expression/sad.png')
+    elif(sel == 4):
+        image = cv2.imread('expression/superise.png')
+
  
     window_name = 'projector'
     cv2.namedWindow(window_name, cv2.WND_PROP_FULLSCREEN)
@@ -18,5 +29,16 @@ if __name__ == '__main__':
     cv2.setWindowProperty(window_name, cv2.WND_PROP_FULLSCREEN,
                           cv2.WINDOW_FULLSCREEN)
     cv2.imshow(window_name, image)
+
+
+
+ 
+if __name__ == '__main__':
+    while True:
+
+        expression_rob(random.randint(1,4))   
+        time.sleep(20)
+
+
     cv2.waitKey()
     cv2.destroyAllWindows()
